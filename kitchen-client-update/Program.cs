2e7833c03b187f9ad0912baf1cb5ae4aa1bf1589
beiguanyi <beiguanyi@ut.cn>
2021-01-20 14:36:26 +0800

    添加项目文件。

---
+++ kitchen-client-update/Program.cs
@@ -0,0 +1,68 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace kitchen_client_update
+{
+    class Program
+    {
+        private static void 更新(string title, string url)
+        {
+            AutoUpdater.CheckForUpdateEvent += (UpdateInfoEventArgs args) =>
+            {
+                MessageBoxResult dialogResult;
+                if (args.Error == null)
+                {
+                    if (args.InstalledVersion >= new Version(args.CurrentVersion))
+                    {
+                        return;
+                    }
+                    if (args.Mandatory.Value)
+                    {
+                        dialogResult = MessageBox.Show($"程序最新版本为{args.CurrentVersion}。您使用的{args.InstalledVersion}版本过旧。可能会无法使用。点确定更新程序。", "强制更新", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        dialogResult = MessageBox.Show($"程序最新版本为{args.CurrentVersion}。您使用的{args.InstalledVersion}版本过旧。是否更新。", "更新", MessageBoxButton.YesNo,
+                        MessageBoxImage.Information);
+                    }
+
+                    if (dialogResult.Equals(MessageBoxResult.Yes) || dialogResult.Equals(MessageBoxResult.OK))
+                    {
+                        try
+                        {
+                            if (AutoUpdater.DownloadUpdate(args))
+                            {
+                                Console.Write("exit");
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show($"更新出错：{exception.Message}，请联系相关人员");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"更新检测出错：{args.Error.Message}，请联系相关人员");
+                }
+            };
+            AutoUpdater.AppTitle = title;
+            AutoUpdater.Synchronous = true;
+            AutoUpdater.AppCastURL = url;
+            AutoUpdater.Start();
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                更新(args[0], args[1]);
+            }
+        }
+    }
+}
